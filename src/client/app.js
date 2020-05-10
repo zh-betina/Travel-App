bd79f52499f8242d0c3665467ba0fcaf705bb449
Betina Jasinska <zh.betina@gmail.com>
2020-05-10 23:42:34 +0200

    Add files.

---
+++ src/client/app.js
@@ -0,0 +1,7 @@
+import './styles/layout.scss';
+import './styles/navbar.scss';
+import './styles/main.scss';
+import './styles/footer.scss';
+import bgr from './media/bgr.png';
+import bgrMobile from './media/bgr-mobile.png';
+import logo from './media/logo.png';

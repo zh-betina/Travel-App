0ce51b03772b552ad6b9cb8c1ed8477970216927
Betina Jasinska <zh.betina@gmail.com>
2020-05-16 22:35:34 +0200

    Import necessary new functions. Add promises and .then.

--- src/client/js/handleInput.js
+++ src/client/js/handleInput.js
@@ -7,7 +7,7 @@ function handleInput(){
 
 const dataArray = [];
 let geonamesRes;
-
+let weatherData;
 const placeName = document.getElementById('place').value;
 const date = document.getElementById('date').value;
 const geonamesUrl = `http://api.geonames.org/searchJSON?q=${placeName}&maxRows=1&username=zh.betina`;
@@ -22,14 +22,35 @@ getAPIdata(geonamesUrl, geonamesRes)
 })
 .then(dataArray=> {
   const getNowDate = new Date();
-  chooseWeatherbitOption(dataArray, getNowDate);
+  daysDiff(dataArray, getNowDate);
+  return dataArray;
 })
+.then(dataArray=> {
+  const coord = `lat=${dataArray[3]}&lon=${dataArray[4]}`;
+  const key = `&key=8b8571fff1e64adeab1381af83e01d85`;
+  const urlCurrent = `https://api.weatherbit.io/v2.0/current?${coord}${key}`;
+  const urlFuture = `https://api.weatherbit.io/v2.0/forecast/daily?${coord}${key}`;
+  let weatherArray = [];
+  if(dataArray[5]<=7 && dataArray[5]>=0){
+    return getAPIdata(urlCurrent, weatherData).then(weatherData=>{
+      sortWeatherData(weatherData, weatherArray);
+    });
+  } else {
+    return getAPIdata(urlFuture, weatherData).then(weatherData=>{
+      sortWeatherData(weatherData, weatherArray);
+    });
+  };
+})
+.then(dataArray=>{
+  console.log('THE END');
+});
 };
 
 
 
 import { getAPIdata } from './getAPIdata.js';
-import { chooseWeatherbitOption } from './chooseWeatherbitOption.js'
+import { daysDiff } from './daysDiff.js';
+import { sortWeatherData } from './sortWeatherData.js';
 
 export { clickEvent }
 export { handleInput }

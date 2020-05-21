e088f18f33635649ee180a8ef77970c560c884d3
Betina Jasinska <zh.betina@gmail.com>
2020-05-21 22:31:06 +0200

    Create POST function.

---
+++ src/client/js/postToServer.js
@@ -0,0 +1,22 @@
+import { handleInput } from './handleInput.js';
+
+async function postToServer(url, data){
+  const response = await fetch(url, {
+    method: 'POST',
+    mode: 'cors',
+    credentials: 'same-origin',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(data)
+  });
+  try{
+    const res = await response.json();
+    console.log(res);
+    return res;
+  }catch(error){
+    console.log('While POSTing, error occured:', error);
+  };
+};
+
+export { postToServer };

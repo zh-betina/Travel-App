39e176a85434181eeeaa34d85e80b7b28bf6a6ec
Betina Jasinska <zh.betina@gmail.com>
2020-05-21 22:30:46 +0200

    Add /post route

--- src/server/server.js
+++ src/server/server.js
@@ -2,18 +2,51 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
-
 const app = express();
+//Amadeus API
+const Amadeus = require('amadeus');
+const amadeus = new Amadeus({
+  clientId:'kysMo0J8DtFooMV1c5icr5DLDGn6S7C8',
+  clientSecret: 'Q62Z4vend2WULdY1',
+  logLevel: 'debug'
+});
 
-app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 app.use(cors());
 
 app.use(express.static('dist'));
 
-app.listen(8081, ()=>{console.log('Runnin on port 8081')});
+
+
+app.listen(8081, ()=>{console.log('Running on port 8081')});
 
 app.get('/', (req,res)=>{
   res.sendFile('dist/index.html');
 });
+
+app.post('/post', dataProcess);
+
+function dataProcess(req, res){
+
+  const test = req.body;
+  console.log(test);
+  res.send('works');
+};
+
+
+/*amadeus.shopping.hotelOffers.get({
+  latitude: ,
+  longitude: ,
+  checkInDate:
+})
+.then(response=>{
+  console.log(response.body);
+  console.log(response.result);
+  console.log(response.data);
+})
+.catch(error=>{
+  console.log(error.response);
+});*/

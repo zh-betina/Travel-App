605ea4a4c92b4f68f80e7c05adae848ea159f265
Betina Jasinska <zh.betina@gmail.com>
2020-05-22 20:34:05 +0200

    Fix sticky footer

--- src/client/js/displayResult.js
+++ src/client/js/displayResult.js
@@ -6,13 +6,32 @@ function displayResult(res, dataSet){
   let address;
   let descr;
   let deal;
-  let aboutHotel;
+
+  let aboutHotel1 =
+  `<h2>You're leaving for ${dataSet.city} in ${dataSet.daysDiff} days.</h2>
+  <h3>Departure date: ${dataSet.date}</h3>
+  <h3>${dataSet.country}</h3>
+  <div id="results">
+  <img id="country-pic" src="${dataSet.picUrl}" alt="Photo of the destination"/>
+  <h3>Forecast</h3>
+  <div id="weather">
+  <p>${dataSet.temp}°C</p>
+  <p>${dataSet.weather}</p>
+  </div>
+  <h2>Top cheapest hotel deals</h2>
+  <div id="hotel-deals">
+  </div>
+  <button id="save" onclick="return Client.saveTrip()">Save this trip</button>
+  <button id="cancel" onclick="return Client.cancel()">Cancel</button>
+  </div>`;
+  document.getElementById('main').innerHTML = aboutHotel1;
 
 
-  for(let i = 0; i <= res.length-1; i++){
+  for(let i = 0; i <= 3; i++){
     let hotelName = [res[i].hotel.name];
-    let adress = [res[i].hotel.address.lines[0], res[i].hotel.address.postalCode,
-                  res[i].hotel.address.cityName];
+    let address = `<p>${res[i].hotel.address.lines[0]}</p>
+    <p>${res[i].hotel.address.postalCode}</p>
+    <p>${res[i].hotel.address.cityName}</p>`;
 
       if(res[i].description==undefined){
         descr = "No description available.";
@@ -21,39 +40,16 @@ function displayResult(res, dataSet){
       }
 
     let deal = [res[i].offers[0].price.total, res[i].offers[0].price.currency];
+    let newEl = document.createElement('div');
+    newEl.innerHTML =
+     `<p>${hotelName}</p>
+      <p>Picture here</p>
+      <p>${descr}</p>
+      <p>${address}</p>
+      <p>Average estimated price per night for 1 adult:</p>
+      <p>${deal}</p>`;
+    document.getElementById('hotel-deals').appendChild(newEl);
   };
-
-  // TODO:
-  //Divide the part to iterate from the part not requiring iteration
-  aboutHotel = `<div id="results">
-  <h2>You're leaving for ${dataSet.city} in ${dataSet.daysDiff} days.</h2>
-  <p>Departure date: ${dataSet.date}</p>
-  <p>${dataSet.countryName}<p>
-  <img id="country-pic" src="${dataSet.picUrl}" alt="Photo of the destination"/>
-  <div id="section">
-  <h3>Forecast</h3>
-  <div id="weather">
-  <p>${dataSet.temp}°C</p>
-  <p>${dataSet.weather}</p>
-  </div>
-  <h4>Top cheapest hotel deals</h4>
-  <div id="hotel-deals">
-  <div class="hotel">
-  <p>${hotelName}</p>
-  <p>Picture here</p>
-  <p>${descr}</p>
-  <p>${address}</p>
-  <p>Average estimated price per night for 1 adult:</p>
-  <p>${deal}</p>
-  </div>
-  </div>
-  </div>
-  <button id="save" onclick="return Client.saveTrip()">Save this trip</button>
-  <button id="cancel" onclick="return Client.cancel()">Cancel</button>
-  </div>`;
-
-  document.getElementById('main').innerHTML = aboutHotel;
-
   return;
 };
 

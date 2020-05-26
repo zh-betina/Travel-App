3dd703abba98393af16e3359fe583defd818ac59
Betina Jasinska <zh.betina@gmail.com>
2020-05-26 22:34:50 +0200

    Update a 'saved trip status info'

--- src/client/js/displayResult.js
+++ src/client/js/displayResult.js
@@ -12,14 +12,16 @@ function displayResult(res, dataSet){
     <img id="country-pic" src="${dataSet.picUrl}" alt="Photo of the destination"/>
     <h3>Forecast</h3>
     <div id="weather">
-    <p>${dataSet.temp}°C</p>
-    <p>${dataSet.weather}</p>
+      <p>${dataSet.temp}°C</p>
+      <p>${dataSet.weather}</p>
     </div>
     <h2>Top cheapest hotel deals</h2>
     <div id="hotel-deals">
     </div>
-    <button id="save" onclick="return Client.saveTrip()">Save this trip</button>
-    <button id="cancel" onclick="return Client.cancel()">Cancel</button>
+    <div id="options-buttons">
+      <button id="save" onclick="return Client.saveTrip()">Save this trip</button>
+      <button id="cancel" onclick="return Client.cancel()">Cancel</button>
+    </div>
   </div>`;
   document.getElementById('main').innerHTML = aboutPlace;
 
--- src/client/js/saveItem.js
+++ src/client/js/saveItem.js
@@ -5,6 +5,9 @@ function saveItem(key, value){
 localStorage.setItem(key, JSON.stringify(value));
 console.log('saved');
 
+document.getElementById('options-buttons').innerHTML =
+"✅ <b>Your trip has been successfully saved.</b> ✅";
+
 }
 
 export { saveItem };

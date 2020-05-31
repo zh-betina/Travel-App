28af2e371c9422436cc49ebacb2624b3dbb6707d
Betina Jasinska <zh.betina@gmail.com>
2020-06-01 00:14:01 +0200

    Replace \n with <br>

--- src/client/js/displayTrips.js
+++ src/client/js/displayTrips.js
@@ -4,23 +4,49 @@ const tripsDiv = document.getElementById('trips');
 
 let storageObject = {};
 let tripsArray = [];
+manageData(storageObject, tripsArray);
 
-for(let i = 0; i <= localStorage.length + 1; i++){
-  let key = localStorage.key(i);
-  let item = localStorage.getItem(key);
-  storageObject = {
-    [key]: item
-  }
+for(let i = 0; i <= tripsArray.length-1; i++){
+  let newEl = document.createElement('div');
+  newEl.classList.add('bgr-box');
 
-  let containsString = Object.keys(storageObject).some(key=>{
-    return /tripInfo/.test(key);
-  });
+  let contentDest = tripsArray[i].destination;
+  let contentPic = tripsArray[i].photo;
+  let contentWeather = tripsArray[i].weather;
+  let contentHotels = tripsArray[i].hotels;
+  newEl.innerHTML = `<p>${contentDest}</p>
+  ${contentPic}
+  <p>${contentWeather}</p>
+  <div class="hotels">
+  <p>${contentHotels}</p>
+  </div>`;
+tripsDiv.appendChild(newEl);
+};
+const tripsDivContent = tripsDiv.innerHTML;
+tripsDiv.innerHTML = tripsDivContent.replace(/\n/g, '<br />');
 
-if(containsString == true){
-  tripsArray.push(JSON.parse(item));
-}
-}
-console.log(tripsArray);
+console.log('done');
+};
+
+//Get from local storage only necessary items:
+function manageData(storageObject, tripsArray){
+
+  for(let i = 0; i <= localStorage.length + 1; i++){
+    let key = localStorage.key(i);
+    let item = localStorage.getItem(key);
+    storageObject = {
+      [key]: item
+    }
+
+    let containsString = Object.keys(storageObject).some(key=>{
+      return /tripInfo/.test(key);
+    });
+
+    if(containsString == true){
+      tripsArray.push(JSON.parse(item));
+    }
+  };
+  return tripsArray;
 };
 
 export { displayTrips };

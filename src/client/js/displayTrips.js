76128f834fa91f95d643d1a5ec20b6d0388da38c
Betina Jasinska <zh.betina@gmail.com>
2020-06-02 20:48:56 +0200

    Insert objects in one array

--- src/client/js/displayTrips.js
+++ src/client/js/displayTrips.js
@@ -1,24 +1,34 @@
+//Global variables:
+let tripsArray = [];
+
+//Functions responsible for trips display in My Trips section:
 function displayTrips(){
 
 const tripsDiv = document.getElementById('trips');
 
 let storageObject = {};
-let tripsArray = [];
 manageData(storageObject, tripsArray);
 
 for(let i = 0; i <= tripsArray.length-1; i++){
   let newEl = document.createElement('div');
-  newEl.classList.add('bgr-box');
+  newEl.classList.add('trip-box');
+  //Getting into the right array:
+  let neededData = tripsArray[i][0];
+  let parsedAr = JSON.parse(neededData[1]);
 
-  let contentDest = tripsArray[i].destination;
-  let contentPic = tripsArray[i].photo;
-  let contentWeather = tripsArray[i].weather;
-  let contentHotels = tripsArray[i].hotels;
+  let contentDest = parsedAr.destination;
+  let contentPic = parsedAr.photo;
+  let contentWeather = parsedAr.weather;
+  let contentHotels = parsedAr.hotels;
   newEl.innerHTML = `<p>${contentDest}</p>
-  ${contentPic}
   <p>${contentWeather}</p>
+  ${contentPic}
   <div class="hotels">
   <p>${contentHotels}</p>
+  </div>
+  <div class="btns-wrap">
+  <button class="btn trip-btn" onclick="return Client.removeEntry(${i})">Remove</button>
+  <button class="btn trip-btn">Add To-Do List</button>
   </div>`;
 tripsDiv.appendChild(newEl);
 };
@@ -29,7 +39,7 @@ console.log('done');
 };
 
 //Get from local storage only necessary items:
-function manageData(storageObject, tripsArray){
+export default function manageData(storageObject, tripsArray){
 
   for(let i = 0; i <= localStorage.length + 1; i++){
     let key = localStorage.key(i);
@@ -43,10 +53,12 @@ function manageData(storageObject, tripsArray){
     });
 
     if(containsString == true){
-      tripsArray.push(JSON.parse(item));
+      const entries = Object.entries(storageObject);
+      tripsArray.push(entries);
     }
   };
   return tripsArray;
 };
 
+export { tripsArray };
 export { displayTrips };

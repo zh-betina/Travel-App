64b40c8905cbc93013a1dcb1ade8daadbd0ebf42
Betina Jasinska <zh.betina@gmail.com>
2020-06-02 20:51:39 +0200

    Initial commit

---
+++ src/client/js/removeEntry.js
@@ -0,0 +1,17 @@
+import { manageData, tripsArray } from './displayTrips.js';
+
+function removeEntry(i, tripsArray){
+
+let localSt= localStorage.length;
+for(let j=0; j<=localSt+1; j++){
+  let key = localStorage.key(j);
+  let array = Client.tripsArray[i][0][0];
+    if(key == array){
+      localStorage.removeItem(key);
+      return location.reload();
+    };
+};
+
+}
+
+export { removeEntry };

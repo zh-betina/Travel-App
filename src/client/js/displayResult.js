4cdfea0067398b26277ff0456a72e4f6252e6ebb
Betina Jasinska <zh.betina@gmail.com>
2020-06-04 15:55:47 +0200

    Modify innerHTMl for result display

--- src/client/js/displayResult.js
+++ src/client/js/displayResult.js
@@ -1,12 +1,17 @@
 import { handleInput } from './handleInput.js';
 
 function displayResult(res, dataSet){
-// TODO: Go to STYLES and adapt styling for esthetic results display
+  let dayS;
+    if(dataSet.daysDiff == 1){
+      dayS = "day"
+    }else{
+      dayS = "days"
+    };
   let aboutPlace =
   `<div id="dest-info">
-    <h2>You're leaving for ${dataSet.city} in ${dataSet.daysDiff} days.</h2>
-    <h3>Departure date: ${dataSet.date}</h3>
-    <h3>${dataSet.country}</h3>
+    <h2>You're leaving for ${dataSet.city}, ${dataSet.country} in ${dataSet.daysDiff} ${dayS}.</h2>
+    <h3>Departure date:</h3>
+    <p>${dataSet.date}</p>
   </div>
   <div id="results">
     <img id="country-pic" src="${dataSet.picUrl}" alt="Photo of the destination"/>
@@ -15,16 +20,16 @@ function displayResult(res, dataSet){
       <p>${dataSet.temp}°C</p>
       <p>${dataSet.weather}</p>
     </div>
-    <h2>Top cheapest hotel deals</h2>
+    <h3>Top cheapest hotel deals</h3>
     <div id="hotel-deals">
     </div>
     <div id="options-buttons">
-      <button id="save" onclick="return Client.saveTrip()">Save this trip</button>
-      <button id="cancel" onclick="window.location='/'">Cancel</button>
+      <button class="btn" id="save" onclick="return Client.saveTrip()">Save this trip</button>
+      <!-- TODO: change this onclick for sth else (<a>? external function?)-->
+      <button class="btn" id="cancel" onclick="window.location='/'">Cancel</button>
     </div>
   </div>`;
   document.getElementById('main').innerHTML = aboutPlace;
-
 
   for(let i = 0; i <= 3; i++){
     if(res[i] == undefined){
@@ -32,14 +37,17 @@ function displayResult(res, dataSet){
     }else{
     let hotelName = [res[i].hotel.name];
     let deal = [res[i].offers[0].price.total, res[i].offers[0].price.currency];
-    let address = `<p>Address: ${res[i].hotel.address.lines[0]}</p>
-    <p>Postal code: ${res[i].hotel.address.postalCode}</p>
+    let address = `<p class="bold">Address:</p>
+    <p>${res[i].hotel.address.lines[0]}</p>
+    <p class="bold">Postal code:</p>
+    <p>${res[i].hotel.address.postalCode}</p>
     <p>${res[i].hotel.address.cityName}</p>`;
     const elContent = `<p>${hotelName}</p>
-      <p>Picture here</p>
+      <i class="fas fa-h-square"></i>
       <p>${address}</p>
       <p>Average estimated price per night for 1 adult:</p>
-      <p>${deal}</p>`;
+      <p>${deal}</p>
+      <hr>`;
 
     let newEl = document.createElement('div');
     newEl.innerHTML = elContent;

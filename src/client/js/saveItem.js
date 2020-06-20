a6ed2d34b7f75a1d801e42d6b169ce2b00a6c4b6
Betina Jasinska <zh.betina@gmail.com>
2020-06-20 21:50:09 +0200

    Add button to go back to home page after trip save

--- src/client/js/saveItem.js
+++ src/client/js/saveItem.js
@@ -6,8 +6,8 @@ localStorage.setItem(key, JSON.stringify(value));
 console.log('saved');
 
 document.getElementById('options-buttons').innerHTML =
-"✅ <b>Your trip has been successfully saved.</b> ✅";
-
+`<p>✅ <b>Your trip has been successfully saved.</b> ✅</p>";
+<button class="btn"><a href="/">Back to searching</a></button>`;
 }
 
 export { saveItem };

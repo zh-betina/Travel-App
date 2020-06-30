b95ea0d5dab7ef6fa81e32e91ecf1365e440f822
Betina Jasinska <zh.betina@gmail.com>
2020-06-30 14:56:36 +0200

    commit

--- src/client/js/getAPIdata.js
+++ src/client/js/getAPIdata.js
@@ -3,8 +3,8 @@ import { handleInput } from './handleInput.js'
 async function getAPIdata(url, data){
     const response = await fetch(url, {
       method: 'GET',
-      //mode: 'no-cors',
-      credentials: 'omit',
+      mode: 'no-cors',
+      credentials: 'same-origin',
       headers: {
         'Content-Type': 'application/json',
       },

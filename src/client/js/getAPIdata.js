c87b421a0b6de673d46304df5464c65b5a141eb7
Betina Jasinska <zh.betina@gmail.com>
2020-06-30 11:06:47 +0200

    testing

--- src/client/js/getAPIdata.js
+++ src/client/js/getAPIdata.js
@@ -3,8 +3,8 @@ import { handleInput } from './handleInput.js'
 async function getAPIdata(url, data){
     const response = await fetch(url, {
       method: 'GET',
-      mode: 'no-cors',
-      credentials: 'omit',
+      mode: 'cors',
+      credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
       },
